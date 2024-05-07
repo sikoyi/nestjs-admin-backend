233e51f1332dfe0a38770cb8c5343a7c488e3b26
yuansheng <2079308565@qq.com>
2024-05-07 17:27:18 +0800

    初始化 mysql redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,48 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createClient } from 'redis';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'task_admin',
+      synchronize: true,
+      logging: true, // 是否打印 sql 语句
+      entities: [], // 表
+      poolSize: 10, // 连接数量
+      connectorPackage: 'mysql2', // 连接池
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // 注册 Redis 模块
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = createClient({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+        });
+
+        await client.connect();
+        return client;
+      },
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('后台管理系统接口')
+    .setDescription('描述: 包含所有接口')
+    .setVersion('v1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
